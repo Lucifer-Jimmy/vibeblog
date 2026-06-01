f588d555e32d3583107728c7379538bfb505d152
Lucifer-Jimmy <lucifer.stars@qq.com>
2026-06-01 15:26:40 +0800

    delete-some-notes

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -13,9 +13,6 @@ class Setting extends Model
 
     protected $fillable = ['key', 'value'];
 
-    /**
-     * 获取设置值，支持默认值
-     */
     public static function get(string $key, mixed $default = null): mixed
     {
         $setting = static::find($key);
@@ -23,9 +20,6 @@ class Setting extends Model
         return $setting ? $setting->value : $default;
     }
 
-    /**
-     * 设置值
-     */
     public static function set(string $key, mixed $value): void
     {
         static::updateOrCreate(['key' => $key], ['value' => $value]);
--- app/helpers.php
+++ app/helpers.php
@@ -3,9 +3,6 @@
 use App\Models\Setting;
 
 if (!function_exists('setting')) {
-    /**
-     * 获取或设置站点配置
-     */
     function setting(string $key, mixed $default = null): mixed
     {
         return Setting::get($key, $default);
